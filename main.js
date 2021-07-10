ff711cd017034126a96e541a483550497bd5715b
Maciej 'Matt' Madejsza <maciej.madejsza@gmail.com>
2021-07-10 13:55:22 +0100

    assign error

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let value = "coding id good";
-let value1 = 1;
-let value2 = 2;
-let value3 = 3;
-let o1 = Object()
-o1.foo = "o1"
-let o2 = Object(value)
+let value1 = "and cool";
+let o1 = Object(value)
+let o11 = Object(value1)
+console.log("o1", o1)
+console.log(o1.charAt(2))
+const o2Assigned = Object.assign(o1, o11)
 
-console.log("o1", o1);
-console.log("o2", o2)
-console.log(o2.charAt(2))
+// Uncaught TypeError: Cannot assign to read only property '0' of object '[object String]'
+// at Function.assign (<anonymous>)
+// {/* at main.js:7 */}
