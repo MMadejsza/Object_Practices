06fd60623b989732f622d36f1b7b5b0d5ec69fd2
Maciej 'Matt' Madejsza <maciej.madejsza@gmail.com>
2021-07-11 21:13:51 +0100

    end for today

--- notki.js
+++ notki.js
@@ -2,5 +2,10 @@ d = Object.getOwnPropertyDescriptor(o, Symbol.for('baz'));
 
 map(([key, value]) => [key, value ** 3]))
 
+Object.is() //??
 
+Object.freeze() "vs" Object.preventExtensions()
 
+configurable, writtable,
+
+    constructor i klasy
