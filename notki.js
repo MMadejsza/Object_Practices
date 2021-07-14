10efd53ec9719ac60cbf867768a85688a45c72ee
Maciej 'Matt' Madejsza <maciej.madejsza@gmail.com>
2021-07-14 21:43:35 +0100

    Powtórka do Find

--- notki.js
+++ notki.js
@@ -11,3 +11,19 @@ configurable, writtable,
     constructor i klasy
 
 Promise i proxy
+
+arr = [1, 2, 3, 4];
+arr.every((elem, index, arr) => {
+    arr.pop()
+    console.log(`[${arr}][${index}] -> ${elem}`)
+    return elem < 4
+})
+
+// Loop runs for 2 iterations only, as the remaining
+// items are `pop()`ed off
+//
+// 1st iteration: [1,2,3][0] -> 1
+// 2nd iteration: [1,2][1] -> 2
+Dlaczego tylko 2 iteracje a nie do 0 ?
+
+    https ://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter nie rozumiem w ogóle tego zastosowania w przykładach i tych returnów
